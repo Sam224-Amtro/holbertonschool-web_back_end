29cd7fc72e8c8ae630c74e1ff1ded37b770298f7
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 11:34:26 +0100

    the file to my to_str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Ce module contient la fonction annotée par type to_str.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Convertit un float en sa représentation sous forme de chaîne.
+
+    Args:
+        n (float): Le flotteur à convertir.
+
+    Returns:
+        str: La représentation sous forme de chaîne du float.
+    """
+    return str(n)
