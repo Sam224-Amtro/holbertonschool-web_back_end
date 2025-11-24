a6171ff132b1543dd70ca1dbd6a4f1d1ce97d246
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 10:12:52 +0100

    my add file was missing a comment

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,6 +3,7 @@
 Module qui contient une simple fonction add(a, b)
 qui renvoie la somme de a et b sous forme de flottant.
 """
+
 def add(a: float, b: float) -> float:
     """Renvoie la somme de deux nombres flottants."""
     return a + b
