14b54858ee96360c6ace635dcd3a29fb0ae3d7e0
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 10:11:53 +0100

    my add file was missing a comment

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+"""
+Module qui contient une simple fonction add(a, b)
+qui renvoie la somme de a et b sous forme de flottant.
+"""
 def add(a: float, b: float) -> float:
     """Renvoie la somme de deux nombres flottants."""
     return a + b
