dba40f3063a7030dd254c1956194de0980a6721b
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 16:01:57 +0100

    the file to my sum_list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Retourne la somme des éléments d'une liste de nombres."""
     return sum(input_list)
