bab8d54e5936e676a106ac105dcc3c2dd218022c
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 10:38:23 +0100

    the file to my concat file

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function concat that takes a string str1 and str2 as arguments"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatène deux chaînes de caractères.
+
+    Args:
+        str1 (str): Première chaîne.
+        str2 (str): Deuxième chaîne.
+
+    Returns:
+        str: Les deux chaînes concaténées.
+    """
+    return str1 + str2
+
