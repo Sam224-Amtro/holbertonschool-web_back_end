b374a0bd76e00755454c6778c80f4a8582b18255
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 15:58:50 +0100

    the file to my sum_list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
-
 from typing import List
-"""
-Import (ici inutilisé) permettant d'annoter des listes, par exemple List[float].
-"""
 
-def sum_list(input_list: list[float]) -> float:
-    """
-    Prend une liste de nombres flottants en entrée et retourne leur somme.
 
-    Paramètres :
-        input_list (list[float]) : la liste des nombres à additionner.
-
-    Retour :
-        float : le résultat de l'addition de tous les éléments.
-    """
+def sum_list(input_list: List[float]) -> float:
+    """Retourne la somme des éléments d'une liste de nombres."""
     return sum(input_list)
