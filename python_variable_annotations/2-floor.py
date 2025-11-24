952bf158e8d982f92620d3db817fc308c26f4f9d
Samamtro <fofanasanoussy545@gmail.com>
2025-11-24 11:09:23 +0100

    the file to my floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
+import math
 
-from math import floor as math_floor
-
-
-def floor(n: float) -> int:
+def floor(n):
     """
-    Renvoie le plancher (floor) du nombre flottant donné.
+    Renvoie le plancher (floor) du nombre n.
 
     Args:
-        n (float): le nombre flottant dont on veut le plancher.
+        n: Le nombre entier ou flottant dont on veut le plancher.
 
     Returns:
         int: Le plus grand entier inférieur ou égal à n.
     """
-    return math_floor(n)
+    # Utilise directement math.floor pour obtenir le plancher
+    return math.floor(n)
