f9dbf3d561888e18e945e781bdf25186009788f2
Samamtro <fofanasanoussy545@gmail.com>
2025-11-25 19:58:35 +0100

    the file to my to_kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Fonction qui convertit une clé et une valeur en un tuple."""
+
 from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
