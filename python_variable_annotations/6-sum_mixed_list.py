0caa79d294994c2cd9ca8e409b08f20901289a78
Samamtro <fofanasanoussy545@gmail.com>
2025-11-25 19:42:42 +0100

    the file to my sum_mixed

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,11 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 """Fonction pour la liste mixte"""
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """Takes a list of integers and floats and returns their sum as a float."""
+    """
+    Prend une liste d'entiers et de nombres à virgule flottante et
+    renvoie leur somme sous forme de nombre à virgule flottante.
+    """
     return float(sum(mxd_lst))
