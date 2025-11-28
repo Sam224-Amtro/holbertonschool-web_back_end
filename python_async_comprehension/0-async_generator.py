40cef93e9ec34361ef488a1f8b8b7d2d963495a8
Samamtro <fofanasanoussy545@gmail.com>
2025-11-28 15:31:58 +0100

    the file to async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-""" Let's execute multiple coroutines at the same time with async """
-
+"""Exécutons plusieurs coroutines en même temps avec async"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Generate a random number every second."""
+    """
+    Générez de manière asynchrone 10 nombres aléatoires.
+
+    Cette coroutine boucle 10 fois, en attendant de manière asynchrone
+    1 seconde à chaque itération, puis donne un flottant aléatoire
+    entre 0 et 10.
+
+    Rendements :
+    float : Un nombre aléatoire dans la plage [0, 10).
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
