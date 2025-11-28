55465789c96a38f3e2528a12fb5bfaf88eb3b215
Samamtro <fofanasanoussy545@gmail.com>
2025-11-28 14:53:30 +0100

    this is the file to my async_generator and README

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Exécutons plusieurs coroutines en même temps avec async """
+
+import asyncio
+import random
+
+async def async_generator():
+    """
+    Génère de façon asynchrone 10 nombres aléatoires entre 0 et 10.
+
+    Yields:
+        float: Un nombre aléatoire compris entre 0 et 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)  # Attend 1 seconde de manière asynchrone
+        yield random.uniform(0, 10)  # Retourne un nombre aléatoire (float) entre 0 et 10
