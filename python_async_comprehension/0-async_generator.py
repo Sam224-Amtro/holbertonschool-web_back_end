9cfd6255a8c8b07fbb44bb9106eb99e7e1948ad7
Samamtro <fofanasanoussy545@gmail.com>
2025-11-28 15:11:50 +0100

    the file to my async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-"""Async generator"""
+# Indique que le script doit être exécuté avec l’interpréteur Python 3.
 
-import asyncio
-import random
+""" Let's execute multiple coroutines at the same time with async """
+# Courte description du script : il montre l'utilisation d'async pour exécuter des coroutines.
+
+import asyncio  # Module permettant de gérer l'exécution asynchrone.
+import random   # Permet de générer des nombres aléatoires.
+import typing   # Sert ici à annoter le type retourné par le générateur.
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:  # type: ignore
     """
-    Generate 10 random numbers between 0 and 10 asynchronously.
+    Générateur asynchrone qui produit un nombre aléatoire entre 0 et 10
+    toutes les secondes, pendant 10 itérations.
     """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+    for _ in range(10):            # Répète l’opération 10 fois.
+        await asyncio.sleep(1)     # Pause asynchrone d'une seconde (n'interrompt pas l'exécution globale du programme).
+        yield random.uniform(0, 10)  # Produit un nombre aléatoire entre 0 et 10.
